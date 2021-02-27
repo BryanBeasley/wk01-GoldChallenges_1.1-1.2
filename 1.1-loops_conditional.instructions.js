d9b7039161708e10367e88e57d1a60e37a6c6153
bryan beasley <beasley.bryan85@gmail.com>
2021-02-27 09:06:13 -0500

    challenges

--- 1.1-loops_conditional.instructions.js
+++ 1.1-loops_conditional.instructions.js
@@ -1,10 +1,43 @@
-/* 
-    1. Write a for loop that counts from 0-10. Each time the loop runs, it should invoke a callback function (see step 2).
+// TODO - 1. Write a for loop that counts from 0-10. Each time the loop runs, it should invoke a callback function (see step 2).
+/* ************************
 
-    2. Declare a function named callback. This function needs to accept a parameter. Inside the function, console.log each number (0-10) from the previously written for loop. Your console should look like the screenshot below.
-        - After you've successfully console.logged 0-10 (as pictured above), move on to step 3.
-        
-    3. Inside of the callback function, write a conditional that checks if the number is even or odd. If the number is even, console.log 'the number is even'. If the number is odd, console.log 'the number is odd'. Your console should now look like the screenshot below.
-*/
+!   for (let i = 0; i <= 10; i++) {
+!   callBack(i);
+!   }
 
-//NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+************************** */
+//TODO 2. Declare a function named callback. This function needs to accept a parameter. Inside the function, console.log each number (0-10) from the previously written for loop. Your console should look like the screenshot below. After you've successfully console.logged 0-10 (as pictured above), move on to step 3.
+/* ************************
+
+! for (let i = 0; i <= 10; i++) {
+!  console.log(i);
+!  callBack(i);
+!   } 
+
+************************** */
+//TODO 3. Inside of the callback function, write a conditional that checks if the number is even or odd. If the number is even, console.log 'the number is even'. If the number is odd, console.log 'the number is odd'. Your console should now look like the screenshot below.
+
+//TODO NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+/* ************************
+
+! for (let i = 0; i <= 10; i++) {
+!  console.log(i);
+!  callBack(i);
+!   } 
+
+************************** */
+
+
+for (i = 0; i <= 10; i++) {
+  console.log(i);
+  callBack(i);
+}
+function callBack(i) {
+  if (i === 0) {
+    console.log("the number is even");
+  } else if (i % 2 === 0) {
+    console.log("the number is even");
+  } else {
+    console.log("the number is odd");
+  }
+}
--- 1.2-arrays-objects.instructions.js
+++ 1.2-arrays-objects.instructions.js
@@ -7,3 +7,50 @@
 */
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+/*//TODO 
+- Favorite movie - 
+    -nameOfMovie
+    -runTime
+    -characters
+    -genre
+*/
+
+let favoriteMovie = {
+            movie: {
+                title: "Willow",
+                runTime: "2h 6min",
+                genre: "fantasy",
+                character: 
+                [ 
+                    {name: 'Madmartigan', age: 29, items: [['sword'], ['black root']]},
+                    {name: 'Willow', age: 17, items: [['sword'], ['black root']]}
+                ] 
+            }
+        }
+
+//console.log(Object.keys(favoriteMovie));
+//console.log(Object.keys(favoriteMovie.movie));
+//console.log(Object.keys(favoriteMovie.movie.toString()));
+console.log(Object.values(favoriteMovie.movie));
+//console.log(Object.values(favoriteMovie.movie.items[1]));
+console.log(favoriteMovie.movie.character[1, );
+
+
+/*//TODO  -2-
+-Two Characters from movie in array items in array within array
+   [
+    { *name: '', *age: '', items: [ [object],[object] ] }
+    { *name: '', *age: '', items: [ [object],[object] ] }
+   ]
+    */
+
+/*//TODO -3-
+-Print out the 
+    -nameOfMovie 
+    -runTime 
+    -the characters
+    -the name of a character 
+    -and one of the items a character has.
+*/
+
